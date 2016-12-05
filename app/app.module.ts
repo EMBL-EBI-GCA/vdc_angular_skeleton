c04a38ce2d6f5b5f656d44f332f8b1318e6b7295
streeter <streeter@ebi.ac.uk>
2016-12-05 14:30:12 +0000

    Example child component

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { CoreModule }  from './core/core.module';
 import { SharedModule }  from './shared/shared.module';
+import { ExampleChildModule } from './example-child/example-child.module';
 
 @NgModule({
-  imports: [ BrowserModule, CoreModule, AppRoutingModule, SharedModule ],
+  imports: [ BrowserModule, CoreModule, AppRoutingModule, SharedModule, ExampleChildModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
---
+++ app/example-child/example-child.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './example-child.component.html',
+})
+export class ExampleChildComponent{ };
